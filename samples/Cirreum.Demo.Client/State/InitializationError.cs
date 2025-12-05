8ae14e1e64340b61f618cbf4c6446cc2196efe68
Glen Banta <glen.banta@outlook.com>
2025-12-05 12:46:51 -0700

    added columns sort background option to datagrid and fixed row header toggle padding.
    improved btn outline secondary color options.

--- samples/Cirreum.Demo.Client/State/InitializationError.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Cirreum.Demo.Client.State;
-
-public record InitializationError {
-	public string StoreName { get; init; } = "";
-	public Exception Exception { get; init; } = null!;
-	public string ErrorMessage { get; init; } = "";
-	public string? StackTrace { get; init; }
-	public DateTime Timestamp { get; init; }
-}
